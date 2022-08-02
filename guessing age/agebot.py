8a90335d447f61143cf9a2c8c5313d48c11e971d
dvrv <daraakojede01@gmail.com>
2022-08-02 18:09:49 +0100

    added .sleep() for a little robotic effect

--- guessing age/agebot.py
+++ guessing age/agebot.py
@@ -1,15 +1,17 @@
 import time
-# need it for the delay
-print('hello there, i was built to read the age of humans\nwould you be delighted in a little mind reading? :) ')
-# little intro
+
+print('hello there, i was built to read the age of humans')
+time.sleep(1)
+print('would you be delighted in a little mind reading? :) ')
+time.sleep(2)
 enter = input('if your answer is yes, enter any english alphabet here: ')
 allowed_entry = 'abcdefghijklmnopqrstuvwxyz'.casefold()
-# unnecessary, i was trying the "not in" function below
+# might be unnecessary, i was trying the "not in" function below
 while len(enter) != 1 or enter not in allowed_entry:
-    enter = input('i\'m sure you didn\'t enter a single english alphabet, let\'s try something else: ')
+    enter = input('i am pretty sure you didn\'t enter a single english alphabet, let\'s try something else: ')
     break
 enter1 = float(input('alright, kindly enter  your shoe size (EU system): '))
-while enter1 not in range(30, 50):
+while enter1 not in range(20, 55):  # average european shoe size
     enter1 = float(input('i\'m afraid that\'s not a valid entry, try that again: '))
     break
 enter2 = int((input('enter the year you were born: ')))
@@ -27,7 +29,7 @@ enter4 = int((20*enter3) + 1021)
 enter5 = abs(int(enter2 - enter4))
 # arithmetics using variables
 print('you are ' + str(enter5) + ' years old, how did i do? :)')
-# line above this one returns a terrible joke
+# line above this one should return a terrible joke(enter5 is a 4-digit number)
 enter6 = int(input('enter 1 for good and 0 for bad: '))
 if enter6 == 1:
     print('i honestly did not expect to pull that off :(\nyou\'re welcome?')
